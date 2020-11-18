2196b09506af4b1a919ee999ec7fe093aaf82296
goncalofonseca <goncalo.fonseca@mindera.com>
2020-11-18 10:43:48 +0000

    simplify http client

--- src/main/java/com/fonseca/breweryapi/client/brewerydb/http/HttpClient.java
+++ src/main/java/com/fonseca/breweryapi/client/brewerydb/http/HttpClient.java
@@ -1,13 +1,10 @@
 package com.fonseca.breweryapi.client.brewerydb.http;
 
 import com.fonseca.breweryapi.client.brewerydb.exception.BreweryNotFoundException;
-import org.apache.http.impl.client.HttpClientBuilder;
-import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.RequestEntity;
-import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
@@ -23,16 +20,10 @@ public class HttpClient {
     @Value("${brewerydb.client.host}")
     private String host;
 
-    private final PoolingHttpClientConnectionManager connectionManager;
-    private final HttpComponentsClientHttpRequestFactory requestFactory;
     private final RestTemplate restTemplate;
 
     public HttpClient() {
-        this.connectionManager = new PoolingHttpClientConnectionManager();
-        org.apache.http.client.HttpClient client =
-                HttpClientBuilder.create().setConnectionManager(connectionManager).build();
-        this.requestFactory = new HttpComponentsClientHttpRequestFactory(client);
-        this.restTemplate = new RestTemplate(requestFactory);
+        this.restTemplate = new RestTemplate();
     }
 
     public <T> T request(RequestEntity<?> requestEntity, Class<T> responseType) {
@@ -63,20 +54,5 @@ public class HttpClient {
         return protocol.concat("://")
                 .concat(host)
                 .concat(path);
-    }
-
-    @Value("${brewerydb.client.connectTimeout:30000}")
-    public void setConnectTimeout(int connectTimeout) {
-        requestFactory.setConnectTimeout(connectTimeout);
-    }
-
-    @Value("${brewerydb.client.readTimeout:240000}")
-    public void setReadTimeout(int readTimeout) {
-        requestFactory.setReadTimeout(readTimeout);
-    }
-
-    @Value("${brewerydb.client.connections.poolSize:10}")
-    public void setConnectionPoolSize(int size) {
-        connectionManager.setMaxTotal(size);
     }
 }
